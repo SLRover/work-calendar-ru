78d84dafb660da021f8659dc20bc9c1e3dbd835f
SLRover <ilya.sapunov@gmail.com>
2019-10-22 11:10:21 +0300

    Version 1.0.2 Updated .gitignore and setup

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='work_calendar_ru',
-    version='1.0.1',
-    packages=['work_calendar_ru', 'work_calendar_ru.data'],
+    version='1.0.2',
+    packages=find_packages(),
     url='https://github.com/SLRover/work-calendar-ru',
     license='MIT',
     author='Ilya Sapunov',
     author_email='ilya.sapunov@gmail.com',
-    description='Russian work calendar'
+    description='Russian work calendar',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
