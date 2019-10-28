cfc0762480984e79a60dcd1d830563606b6c706f
SLRover <ilya.sapunov@gmail.com>
2019-10-28 12:11:25 +0300

    Version 1.0.6 Updated python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='work_calendar_ru',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     url='https://github.com/SLRover/work-calendar-ru',
     license='MIT',
@@ -19,5 +19,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
