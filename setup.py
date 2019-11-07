12a0366b905eee6184462446df2112e2c6cdf786
SLRover <ilya.sapunov@gmail.com>
2019-11-07 23:47:33 +0300

    Version 1.1.1 Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='work_calendar_ru',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     url='https://github.com/SLRover/work-calendar-ru',
     license='MIT',
