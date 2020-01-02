623b03713c4fed20790cecfe7cc1e09ab7e313ca
SLRover <ilya.sapunov@gmail.com>
2020-01-03 02:54:49 +0300

    Updated PyPi release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='work_calendar_ru',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     url='https://github.com/SLRover/work-calendar-ru',
     license='MIT',
