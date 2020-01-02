88b6356409dd7f1294670d9479af57a53be1e9b9
SLRover <ilya.sapunov@gmail.com>
2020-01-03 02:41:29 +0300

    Update calendar data for the 2020

--- work_calendar_ru/data/holidays.py
+++ work_calendar_ru/data/holidays.py
@@ -3,15 +3,15 @@
 import datetime
 
 holidays = [
-    datetime.date(2019, 1, 1), datetime.date(2019, 1, 2), datetime.date(2019, 1, 3), datetime.date(2019, 1, 4),
-    datetime.date(2019, 1, 5), datetime.date(2019, 1, 6), datetime.date(2019, 1, 7), datetime.date(2019, 1, 8),
-    datetime.date(2019, 3, 8), datetime.date(2019, 5, 1), datetime.date(2019, 5, 2), datetime.date(2019, 5, 3),
-    datetime.date(2019, 5, 9), datetime.date(2019, 6, 12), datetime.date(2019, 11, 4)
+    datetime.date(2020, 1, 1), datetime.date(2020, 1, 2), datetime.date(2020, 1, 3), datetime.date(2020, 1, 4),
+    datetime.date(2020, 1, 5), datetime.date(2020, 1, 6), datetime.date(2020, 1, 7), datetime.date(2020, 1, 8),
+    datetime.date(2020, 2, 24), datetime.date(2020, 3, 9), datetime.date(2020, 5, 1), datetime.date(2020, 5, 4),
+    datetime.date(2020, 5, 5), datetime.date(2020, 5, 11), datetime.date(2020, 6, 12), datetime.date(2020, 11, 4)
 ]
 
 short_days = [
-    datetime.date(2019, 2, 22), datetime.date(2019, 3, 7), datetime.date(2019, 4, 30), datetime.date(2019, 5, 8),
-    datetime.date(2019, 6, 11), datetime.date(2019, 12, 31)
+    datetime.date(2020, 4, 30), datetime.date(2020, 5, 8), datetime.date(2020, 6, 11), datetime.date(2020, 11, 3),
+    datetime.date(2020, 12, 31)
 ]
 
 work_weekends = []
